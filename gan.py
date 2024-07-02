05533f1d9228b9c2d828f99a06a3ba36163d84b6
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-07-02 20:45:37 +0530

    lots of change

--- gan.py
+++ gan.py
@@ -1,39 +1,96 @@
 import torch
 import torch.nn as nn
 
-class Generator(nn.Module):
-    def __init__(self, z_dim = 64, hidden_dim = 128) -> None:
+
+
+class Generator(nn.Module) :
+    def __init__(self, z_dim : int = 10, hidden_dim : int = 64, im_channel : int = 1) :
+        """
+        z_dim - noise dimension
+        hidden_dim - parameter can tune as needed
+        im_channel - number of output channels of the generated image 1 for grayscale and 3 for rgb
+        """
         super().__init__()
-        self.linear1 = nn.Linear(z_dim, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, hidden_dim * 2)
-        self.linear3 = nn.Linear(hidden_dim * 2, hidden_dim * 4)
-        self.linear4 = nn.Linear(hidden_dim * 4, hidden_dim * 8)
-        self.last = nn.Linear(hidden_dim*8, 784)
-        self.act = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
+        self.block = nn.Sequential(
+            self.gen_block(z_dim, hidden_dim * 4, 3, 2),             # 1 - 3
+            self.gen_block(hidden_dim * 4, hidden_dim * 2, 4, 1),    # 3 - 6
+            self.gen_block(hidden_dim * 2, hidden_dim, 3, 2),        # 6 - 13
+        )
+        self.final= nn.ConvTranspose2d(hidden_dim, im_channel, 4, 2)  # 13 - 28
+        self.act = nn.Tanh()                                      # Not sure about this
 
-    def forward(self, x : torch.Tensor):
-        x = self.act(self.linear1(x))
-        x = self.act(self.linear2(x))
-        x = self.act(self.linear3(x))
-        x = self.act(self.linear4(x))
-        x = self.last(x)
-        x = self.sigmoid(x)
-        return x
-
-class Discriminator(nn.Module):
-    def __init__(self, hidden_dim = 128) -> None:
-        super().__init__()
-        self.linear1 = nn.Linear(784, hidden_dim * 4)
-        self.linear2 = nn.Linear(hidden_dim * 4, hidden_dim * 2)
-        self.act = nn.LeakyReLU(0.2)
-        self.pred = nn.Linear(hidden_dim*2, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x : torch.Tensor): 
-        x = self.act(self.linear1(x))
-        x = self.act(self.linear2(x))
-        x = self.pred(x)
-        x =  self.sigmoid(x)
+    def gen_block(self, in_dim, out_dim, kernel_size : int  = 3, stride : int = 1, padding : str|int = 'valid') :
+        """
+        out_height = strides[0] * (in_height - 1) + kernel_size[0] - 2 * padding_height + dilation[0] * (kernel_size[0] - 1) + output_padding[0] + 1
+        out_width  = strides[1] * (in_width - 1) + kernel_size[1] - 2 * padding_width + dilation[1] * (kernel_size[1] - 1) + output_padding[1] + 1
+        """
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride),
+            nn.BatchNorm2d(out_dim),
+            nn.ReLU()
+        )
+    
+    def forward(self, x):
+        batch_size, z_dim = x.shape[0], x.shape[1]
+        x= x.view(batch_size, z_dim, 1, 1)
+        x = self.block(x)
+        x = self.final(x)
+        x = self.act(x)
         return x
     
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels : int = 1, hidden_dim : int = 16):
+        super().__init__()
+        """
+        only works for 28 x 28
+        """
+        self.block = nn.Sequential(
+            self.dis_block(in_channels, hidden_dim, 4, 2),                       
+            self.dis_block(hidden_dim, hidden_dim * 2, 4, 2),                          
+            self.dis_block(hidden_dim * 2, 1, 4, 2),
+        )                
+        self.act = nn.Sigmoid()
+
+    def dis_block(self, in_dim, out_dim, kernel_size : int  = 3, stride : int = 1, padding : str|int = 'valid'):
+        """
+        out_height = (in_height + 2 * padding_height - dilation[0] * (kernel_size[0] - 1) - 1 ) / stride[0]  + 1
+        out_width = (in_width + 2 * padding_width - dilation[1] * (kernel_size[1] - 1) - 1 ) / stride[1]  + 1
+        """
+        return nn.Sequential(
+            nn.Conv2d(in_dim, out_dim, kernel_size, stride),
+            nn.BatchNorm2d(out_dim),
+            nn.LeakyReLU(0.2)
+        )
+    
+    def forward(self, x):
+        batch_size = x.shape[0]
+        x = self.block(x)
+        x = x.view(batch_size, -1)
+        x = self.act(x)
+        return x
+    
+
+if __name__=="__main__":
+
+    x = torch.rand(32 , 1, 28, 28)
+
+    dis = Discriminator()
+    gen = Generator()
+
+    sum  = 0
+
+    for i in dis.parameters():
+        sum += i.numel()
+    print(sum)
+
+    sum  = 0
+
+    for i in gen.parameters():
+        sum += i.numel()
+    print(sum)
+
+    x = dis(x)
+
+    print(x.shape)
+        
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import torch.optim
 import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
 import torch
 from gan import Generator, Discriminator
 import matplotlib.pyplot as plt
@@ -7,68 +9,71 @@ import torch.nn.functional as f
 import cv2
 
 
-def draw_sample(z):
-    noise = torch.rand(batch_size, z).to(device)
+def draw_sample(z, ii = 1):
+    noise = torch.randn(batch_size, z).to(device)
     out = gen(noise)
-    out = out[0]
-    out = out.view(28, -1)
-    print(out.shape)
-    plt.imshow(out.detach().cpu().numpy(), cmap='Greys')
-    plt.show()
+    
+    for i in range(ii):
+        outs = out[i]
+        #out = out.view(28, -1)
+        outs = outs.squeeze(0)
+        plt.imshow(outs.detach().cpu().numpy(), cmap='Greys')
+        plt.show()
 
 
 torch.cuda.empty_cache()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-data = torchvision.datasets.MNIST("dataset/", download= True)
+batch_size = 128
 
-batch_size = 512
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
 
-
-imgs = data.data
-
-imgs = imgs/255
-
-plt.imshow(imgs[0].numpy(), cmap='Greys')
-plt.show()
-
+dataloader = DataLoader(
+    torchvision.datasets.MNIST("dataset/", download=False, transform=transform),
+    batch_size=batch_size,
+    shuffle=True)
 
 b = 0
 
 
-
-
-lr= 0.00001
+lr = 0.0002
 z = 64
 
 gen = Generator(z_dim=z).to(device)
+beta_1 = 0.5 
+beta_2 = 0.999
 dis = Discriminator().to(device)
 
-optimizer_gen = torch.optim.Adam(gen.parameters(), lr= lr)
-optimizer_dis = torch.optim.Adam(dis.parameters(), lr= lr)
-
-
-
+optimizer_gen = torch.optim.Adam(gen.parameters(), lr= lr, betas=(beta_1, beta_2))
+optimizer_dis = torch.optim.Adam(dis.parameters(), lr= lr, betas=(beta_1, beta_2))
 
 draw_sample(z)
 
-epochs = 10
+epochs = 20
 
 for i in range(epochs):
 
     b = 0
 
+    print(f"Epoch:{i}")
+
     while b + batch_size  < imgs.shape[0] : 
-        real = imgs[b: b + batch_size].to(device).float()
-        real = real.view(batch_size, -1)
+        optimizer_dis.zero_grad()  
+        real = imgs[b: b + batch_size].to(device).float().unsqueeze(1)
+        #real = real.view(batch_size, -1)
         real_labels = torch.ones(batch_size, 1).to(device)
 
-        noise = torch.rand(batch_size, z).to(device)
+        noise = torch.randn(batch_size, z).to(device)
         fake = gen(noise)
+
         fake_labels = torch.zeros(batch_size, 1).to(device)
         
         fake_pred = dis(fake.detach())
+    
         loss1 = f.binary_cross_entropy(fake_pred, fake_labels)
         
         real_preds = dis(real)
@@ -76,17 +81,18 @@ for i in range(epochs):
 
         loss_d = (loss1 + loss2)/2
 
-        optimizer_dis.zero_grad()  
         loss_d.backward()
         optimizer_dis.step()
 
-        noise = torch.rand(batch_size, z).to(device)
+
+        optimizer_gen.zero_grad() 
+        noise = torch.randn(batch_size, z).to(device)
         fake2 = gen(noise)
         fake_pred2 = dis(fake2)
 
         loss_g = f.binary_cross_entropy(fake_pred2, torch.ones(batch_size, 1).to(device))
 
-        optimizer_gen.zero_grad() 
+        #loss_g = torch.log(1-fake_pred2).mean()
         loss_g.backward()    
         optimizer_gen.step()
 
@@ -95,7 +101,7 @@ for i in range(epochs):
         b += batch_size
 
 
-draw_sample(z)
+draw_sample(z, 10)
 
 
 
