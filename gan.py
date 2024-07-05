be2cf7a7d5411da525dc2f5fe2d14bdd1492f26d
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-07-05 19:02:55 +0530

    complete module restructure

--- gan.py
+++ gan.py
@@ -2,95 +2,214 @@ import torch
 import torch.nn as nn
 
 
-
-class Generator(nn.Module) :
-    def __init__(self, z_dim : int = 10, hidden_dim : int = 64, im_channel : int = 1) :
-        """
-        z_dim - noise dimension
-        hidden_dim - parameter can tune as needed
-        im_channel - number of output channels of the generated image 1 for grayscale and 3 for rgb
-        """
+class ConvBlock(nn.Module):
+    """
+    A basic Conv block with a
+    -> Conv2d
+    -> BatchNorm
+    -> LeakyReLU
+    """
+    def __init__(self, in_dim : int, out_dim : int, kernel_size : int, stride : int, padding : int|str = 'valid', conv_only : bool = False):
         super().__init__()
-        self.block = nn.Sequential(
-            self.gen_block(z_dim, hidden_dim * 4, 3, 2),             # 1 - 3
-            self.gen_block(hidden_dim * 4, hidden_dim * 2, 4, 1),    # 3 - 6
-            self.gen_block(hidden_dim * 2, hidden_dim, 3, 2),        # 6 - 13
-        )
-        self.final= nn.ConvTranspose2d(hidden_dim, im_channel, 4, 2)  # 13 - 28
-        self.act = nn.Tanh()                                      # Not sure about this
+        self.conv = nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias= False)
+        self.conv_only = conv_only
+        if not self.conv_only : 
+            self.norm = nn.BatchNorm2d(out_dim, affine= False)       # affine = false, sets scale to 1 and bias to 0 as suggested by the paper
+            self.act = nn.LeakyReLU(0.2)                             #DCGAN paper uses 0.2 for leakyrelu
 
-    def gen_block(self, in_dim, out_dim, kernel_size : int  = 3, stride : int = 1, padding : str|int = 'valid') :
-        """
-        out_height = strides[0] * (in_height - 1) + kernel_size[0] - 2 * padding_height + dilation[0] * (kernel_size[0] - 1) + output_padding[0] + 1
-        out_width  = strides[1] * (in_width - 1) + kernel_size[1] - 2 * padding_width + dilation[1] * (kernel_size[1] - 1) + output_padding[1] + 1
-        """
-        return nn.Sequential(
-            nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride),
-            nn.BatchNorm2d(out_dim),
-            nn.ReLU()
-        )
+    def forward(self, x : torch.Tensor):
+        x = self.conv(x)
+        if not self.conv_only : 
+            x = self.norm(x)
+            x = self.act(x)
+        return x
     
-    def forward(self, x):
-        batch_size, z_dim = x.shape[0], x.shape[1]
-        x= x.view(batch_size, z_dim, 1, 1)
-        x = self.block(x)
-        x = self.final(x)
-        x = self.act(x)
+class ConvTransposeBlock(nn.Module):
+    """
+    A basic Conv Transpose block with a
+    -> ConvTranspose2d
+    -> BatchNorm
+    -> ReLU
+    """
+    def __init__(self, in_dim : int, out_dim : int, kernel_size : int , stride : int, padding : int  = 0, out_padding : int = 0, convt_only : bool = False):
+        super().__init__()
+        self.convt = nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride, padding, out_padding, bias= False)
+        self.convt_only = convt_only
+        if not self.convt_only :
+            self.norm = nn.BatchNorm2d(out_dim, affine= False)        # affine = false, sets scale to 1 and bias to 0 as suggested by the paper
+            self.act = nn.ReLU()
+
+    def forward(self, x : torch.Tensor):
+        x = self.convt(x)
+        if not self.convt_only :
+            x = self.norm(x)
+            x = self.act(x)
         return x
     
 
-class Discriminator(nn.Module):
-    def __init__(self, in_channels : int = 1, hidden_dim : int = 16):
+class BaseGenerator(nn.Module):
+    """
+    Not to be called directly
+    It is a base class for Generators of various input sizes
+    """
+    def __init__(self) :
         super().__init__()
-        """
-        only works for 28 x 28
-        """
-        self.block = nn.Sequential(
-            self.dis_block(in_channels, hidden_dim, 4, 2),                       
-            self.dis_block(hidden_dim, hidden_dim * 2, 4, 2),                          
-            self.dis_block(hidden_dim * 2, 1, 4, 2),
-        )                
+        self.gen_block = None
+        # TODO : try using sigmoid, maybe ?
+        self.act = nn.Tanh()                         
+
+    def _init_weights(self, module):
+        if isinstance(module , nn.ConvTranspose2d):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)   # DCGAN paper suggest this weight initialization
+            if module.bias is not None :
+                torch.nn.init.zeros_(module.bias) 
+
+    def forward(self, x : torch.Tensor):
+        x = x.unsqueeze(-1).unsqueeze(-1)                                  # from (B, z_dim) to (B, z_dim , 1, 1), basically a 1 x 1 image
+        x = self.gen_block(x)
+        x = self.act(x)
+        return x
+    
+    
+class BaseDiscriminator(nn.Module):
+    """
+    Not to be called directly
+    It is a base class for Discriminators of various input sizes
+    """
+    def __init__(self):
+        super().__init__()
+        self.dis_block = None
         self.act = nn.Sigmoid()
 
-    def dis_block(self, in_dim, out_dim, kernel_size : int  = 3, stride : int = 1, padding : str|int = 'valid'):
-        """
-        out_height = (in_height + 2 * padding_height - dilation[0] * (kernel_size[0] - 1) - 1 ) / stride[0]  + 1
-        out_width = (in_width + 2 * padding_width - dilation[1] * (kernel_size[1] - 1) - 1 ) / stride[1]  + 1
-        """
-        return nn.Sequential(
-            nn.Conv2d(in_dim, out_dim, kernel_size, stride),
-            nn.BatchNorm2d(out_dim),
-            nn.LeakyReLU(0.2)
-        )
-    
-    def forward(self, x):
-        batch_size = x.shape[0]
-        x = self.block(x)
-        x = x.view(batch_size, -1)
+    def _init_weights(self, module):
+        if isinstance(module, nn.Conv2d) :
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)  # DCGAN paper suggest this weight initialization
+            if module.bias is not None :
+                torch.nn.init.zeros_(module.bias)
+
+    def forward(self, x : torch.Tensor):
+        x = self.dis_block(x)
+        x = x.squeeze(-1).squeeze(-1)                                     # from (B, 1, 1, 1) to (B , 1), probabiltiy prediciton for the batch 
         x = self.act(x)
         return x
+    
+class DcGanX128Generator(BaseGenerator):
+    """
+    Generator for 128 x 128 images
+    """
+    def __init__(self, z_dim : int = 100, im_channel : int = 3, hidden_dim : int = 1024):
+        super().__init__()
+        self.gen_block = nn.Sequential(
+            ConvTransposeBlock(z_dim, hidden_dim, 4, 1),                                    #  1 ---> 4
+            ConvTransposeBlock(hidden_dim, hidden_dim//2, 4, 2, 1),                         #  4 ---> 8
+            ConvTransposeBlock(hidden_dim//2, hidden_dim//4, 4, 2, 1),                      #  8 ---> 16
+            ConvTransposeBlock(hidden_dim//4, hidden_dim//8, 4, 2, 1),                      # 16 ---> 32
+            ConvTransposeBlock(hidden_dim//8, hidden_dim//16, 4, 2, 1),                     # 32 ---> 64
+            ConvTransposeBlock(hidden_dim//16, im_channel, 4, 2, 1, convt_only=True),       # 64 ---> 128
+        )
+        self.apply(self._init_weights)
+
+
+class DcGanX128Discriminator(BaseDiscriminator):
+    """
+    Discriminator for 128 x 128 images
+    """
+    def __init__(self, im_channel : int= 3, hidden_dim: int = 1024):
+        super().__init__()
+        self.dis_block = nn.Sequential(
+            ConvBlock(im_channel, hidden_dim//16, 4, 2, 1, conv_only=True),             #128 ----> 64
+            nn.LeakyReLU(0.2),                                                          # DCGAN paper suggested not to use Batchnorm in first block    
+            ConvBlock(hidden_dim//16, hidden_dim//8, 4, 2, 1),                          # 64 ----> 32
+            ConvBlock(hidden_dim//8, hidden_dim//4, 4, 2, 1),                           # 32 ----> 16
+            ConvBlock(hidden_dim//4, hidden_dim//2, 4, 2, 1),                           # 16 ----> 8
+            ConvBlock(hidden_dim//2, hidden_dim, 4, 2, 1),                              #  8 ----> 4
+            ConvBlock(hidden_dim , 1, 4, 1, conv_only= True),                           #  4 ----> 1
+        )
+        self.apply(self._init_weights)
+
+
+class DcGanX64Generator(BaseGenerator):
+    """
+    Generator for 64 x 64 images
+    """
+    def __init__(self, z_dim : int = 100, im_channel : int = 3, hidden_dim : int = 1024):
+        super().__init__()
+        self.gen_block = nn.Sequential(
+            ConvTransposeBlock(z_dim, hidden_dim, 4, 1),                                    #  1 ---> 4
+            ConvTransposeBlock(hidden_dim, hidden_dim//2, 4, 2, 1),                         #  4 ---> 8
+            ConvTransposeBlock(hidden_dim//2, hidden_dim//4, 4, 2, 1),                      #  8 ---> 16
+            ConvTransposeBlock(hidden_dim//4, hidden_dim//8, 4, 2, 1),                      # 16 ---> 32
+            ConvTransposeBlock(hidden_dim//8, im_channel, 4, 2, 1, convt_only=True),        # 32 ---> 64
+        )
+        self.apply(self._init_weights)
+    
+
+class DcGanX64Discriminator(BaseDiscriminator):
+    """
+    Discriminator for 6 x 64 images
+    """
+    def __init__(self, im_channel : int= 3, hidden_dim: int = 1024):
+        super().__init__()
+        self.dis_block = nn.Sequential(
+            ConvBlock(im_channel, hidden_dim//8, 4, 2, 1, conv_only=True),              # 64 ----> 32
+            nn.LeakyReLU(0.2),
+            ConvBlock(hidden_dim//8, hidden_dim//4, 4, 2, 1),                           # 32 ----> 16
+            ConvBlock(hidden_dim//4, hidden_dim//2, 4, 2, 1),                           # 16 ----> 8
+            ConvBlock(hidden_dim//2, hidden_dim, 4, 2, 1),                              #  8 ----> 4
+            ConvBlock(hidden_dim , 1, 4, 1, conv_only= True),                           #  4 ----> 1
+        )
+        self.apply(self._init_weights)
+    
+
+class DcGanX32Generator(BaseGenerator):
+    """
+    Generator for 32 x 32 images
+    """
+    def __init__(self, z_dim : int = 100, im_channel : int = 3, hidden_dim : int = 512):
+        super().__init__()
+        self.gen_block = nn.Sequential(
+            ConvTransposeBlock(z_dim, hidden_dim, 4, 1),                                       #  1 ---> 4
+            ConvTransposeBlock(hidden_dim, hidden_dim//2, 4, 2, 1),                         #  4 ---> 8
+            ConvTransposeBlock(hidden_dim//2, hidden_dim//4, 4, 2, 1),                      #  8 ---> 16
+            ConvTransposeBlock(hidden_dim//4, im_channel, 4, 2, 1, convt_only= True),       # 16 ---> 32
+        )
+        self.apply(self._init_weights)
+    
+
+class DcGanX32Discriminator(BaseDiscriminator):
+    """
+    Discriminator for 32 x 32 images
+    """
+    def __init__(self, im_channel : int = 3, hidden_dim : int = 512) :
+        super().__init__()
+        self.dis_block = nn.Sequential( 
+            ConvBlock(im_channel, hidden_dim//4, 4, 2, 1),                              # 32 ----> 16
+            ConvBlock(hidden_dim//4, hidden_dim//2, 4, 2, 1),                           # 16 ----> 8
+            ConvBlock(hidden_dim//2, hidden_dim, 4, 2, 1),                              #  8 ----> 4
+            ConvBlock(hidden_dim , 1, 4, 1, conv_only= True),                           #  4 ----> 1
+        )
+        self.apply(self._init_weights)
     
 
 if __name__=="__main__":
+    """
+    testing models
+    """
+    torch.cuda.random.manual_seed(42)
+    torch.manual_seed(42)
+    torch.random.manual_seed(42)
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
 
-    x = torch.rand(32 , 1, 28, 28)
+    dis_in = torch.randn(32 , 3, 128, 128).to(device)
+    gen_in = torch.randn(32,100).to(device)
 
-    dis = Discriminator()
-    gen = Generator()
+    gen = DcGanX128Generator(100, 3).to(device)
+    dis = DcGanX128Discriminator(3).to(device)
 
-    sum  = 0
+    x = gen(gen_in)
+    y = dis(dis_in)
 
-    for i in dis.parameters():
-        sum += i.numel()
-    print(sum)
-
-    sum  = 0
-
-    for i in gen.parameters():
-        sum += i.numel()
-    print(sum)
-
-    x = dis(x)
-
-    print(x.shape)
-        
+    print(x.shape)  
+    print(y.shape)    
